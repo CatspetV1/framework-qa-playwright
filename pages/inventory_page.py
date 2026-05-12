6609b0b44215b2a031510018f75225e226b85477
catspet <donneko234@gmail.com>
2026-05-12 13:32:58 -0600

    Fix: Corregir selector del carrito en el inventario

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -6,7 +6,7 @@ class InventoryPage:
         # Localizadores específicos de la tienda
         self.titulo_pagina = page.locator(".title")
         self.boton_add_backpack = page.locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]")
-        self.carrito_badge = page.locator(".shopping_cart_badge_INVENTADO")
+        self.carrito_badge = page.locator(".shopping_cart_badge")
 
     def agregar_mochila(self):
         self.boton_add_backpack.click()
